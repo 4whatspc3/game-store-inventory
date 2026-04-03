ee959bad5c7ba6abf035fb204b06219abf2e36a4
Matheus Augusto <69812698+4whatspc3@users.noreply.github.com>
2026-04-03 13:56:44 -0300

    feat: add a custom request loggin middleware

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 import express from 'express';
 import path from 'path';
+import logger from './middleware/logger.js';
 import homeRouter from './routes/homeRouter.js';
 
 const app = express();
 
 app.set("views", path.join(process.cwd(), "views"));
 app.set("view engine", "ejs");
+
+app.use(logger);
 
 app.use("/", homeRouter);
 
