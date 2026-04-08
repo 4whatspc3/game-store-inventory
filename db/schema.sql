70dabe946f496cac2149b15fbc10621179f688e3
Matheus Augusto <69812698+4whatspc3@users.noreply.github.com>
2026-04-08 13:27:06 -0300

    feat: Add database schema

---
+++ db/schema.sql
@@ -0,0 +1,20 @@
+CREATE TABLE genres (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(100) NOT NULL UNIQUE
+);
+
+CREATE TABLE games (
+    id SERIAL PRIMARY KEY,
+    title VARCHAR(255) NOT NULL,
+    description TEXT,
+    release_date DATE,
+    price DECIMAL(10, 2) NOT NULL,
+    stock INTEGER NOT NULL DEFAULT 0,
+    image_url TEXT
+);
+
+CREATE TABLE games_genres (
+    game_id INTEGER REFERENCES games(id) ON DELETE CASCADE,
+    genre_id INTEGER REFERENCES genres(id) ON DELETE CASCADE,
+    PRIMARY KEY (game_id, genre_id)
+);
