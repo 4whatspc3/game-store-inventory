cf1cd6cfca91c0ba1d4c250712ac0ce0c241cf1a
Matheus Augusto <69812698+4whatspc3@users.noreply.github.com>
2026-07-17 15:09:52 -0300

    fix: pass selectedGenre to views and fix filter dropdown redirection

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -7,7 +7,7 @@ const homeController = async (req, res, next) => {
         const games = await getAllGames();
         const genres = await getAllGenres();
 
-        res.render("home/index", { games, genres });
+        res.render("home/index", { games, genres, selectedGenre: '' });
     } catch (error) {
         next(error)
     }
@@ -21,7 +21,9 @@ const genreController = async (req, res, next) => {
         const games = await getGamesByGenre(id);
         const genres = await getAllGenres();
 
-        res.render("home/genre", { games, genre, genres });
+        const selectedGenre = genre ? genre.name : '';
+
+        res.render("home/genre", { games, genre, genres, selectedGenre });
     } catch (error) {
         next(error);
     }
